916d90da7b650792cd12df90cd01979a97b67403
Sychev Eugene <dev.stratus@gmail.com>
2024-06-07 23:50:50 +0300

    вынесли функции в классы клиенты

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,13 +1,15 @@
-import pprint
-
-import requests
 from json import loads
-
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mailhog_api import MailhogApi
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi
-    login = 's-test2'
+    account_api = AccountApi(host='5.63.153.31:5051')
+    login_api = LoginApi(host='5.63.153.31:5051')
+    mailhog_api = MailhogApi(host='5.63.153.31:5025')
+
+    login = 's-test3'
     password = '12345678'
     email = f'{login}@m.ru'
     json_data = {
@@ -16,14 +18,14 @@ def test_post_v1_account():
         'password': password
     }
 
-    response = post_v1_account(json_data)
+    response = account_api.post_v1_account(json_data=json_data)
     print(response.status_code)
     print(response.text)
 
     assert response.status_code == 201, f"Пользователь не создан {response.json()}"
     # Получить письма из почтового сервера
 
-    response = get_api_v2_messages(response)
+    response = mailhog_api.get_api_v2_messages()
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, "Письма не получены"
@@ -34,7 +36,7 @@ def test_post_v1_account():
     assert token is not None, f"Токен для пользователя {login}, не был получен"
 
     # Активация пользователя
-    response = put_v1_account_token(token)
+    response = account_api.put_v1_account_token(token=token)
     print(response.status_code)
     print(response.text)
 
@@ -47,16 +49,10 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = post_v1_account_login(json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, "Пользователь не был авторизован"
-
-
-
-
-
-
 
 
 def get_activation_token_by_login(login, response):

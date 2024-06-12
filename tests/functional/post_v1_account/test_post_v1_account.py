79ec3c278f85df0c52e9234374921870fea48e7f
Sychev Eugene <dev.stratus@gmail.com>
2024-06-13 00:14:44 +0300

    добавили configuration

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+from restclient.configuration import Configuration as MailhogConfiguration, Configuration as DmApiConfiguration
 import structlog
 
 structlog.configure(
@@ -13,9 +14,12 @@ structlog.configure(
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025', disable_log=True)
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mailhog_api = MailhogApi(configuration=mailhog_configuration)
 
     now_date = datetime.now()
     login = f"s-test-{now_date.strftime('%Y%m%d%H%M%S')}"
